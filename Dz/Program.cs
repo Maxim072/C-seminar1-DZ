3498ec9a01d64733460c4a518da30320418f3e76
Maks <maksx_x_x@mail.ru>
2022-03-01 00:23:14 +0700

    Добавли простой код

--- Dz/Program.cs
+++ Dz/Program.cs
@@ -6,11 +6,26 @@ Console.WriteLine("ввведите число b");
 int numberB = Convert.ToInt32(Console.ReadLine());
 if (numberA > numberB)
 {
-Console.WriteLine ("max = " + numberA);
-Console.WriteLine ("min = " + numberB);
+    Console.WriteLine("max = " + numberA);
+    Console.WriteLine("min = " + numberB);
 }
 else
 {
-Console.WriteLine ("max = " + numberB);
-Console.WriteLine ("min = " + numberA);
+    Console.WriteLine("max = " + numberB);
+    Console.WriteLine("min = " + numberA);
+}
+
+// простой код
+
+int namber1 = 9;
+int namber2 = 12;
+if (namber1 < namber2)
+{
+    Console.WriteLine("меньшее число " + namber1);
+    Console.WriteLine("большее число " + namber2);
+}
+else
+{
+    Console.WriteLine("меньшее число " + namber2);
+    Console.WriteLine("большее число " + namber1);
 }
